0a4bab4c68e8cd7191f8d767353b454dbf0f7f53
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2026-04-19 21:34:26 -0400

    Incorpora instancias a rutas de area

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const contratistaRoutes = require('./routes/contratistaRoutes');
+const areaRoutes = require('./routes/areaRoutes');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/contratistas', contratistaRoutes);
+app.use('/api/areas', areaRoutes);
 
 app.get('/', (req, res) => {
   res.send('API de Gestión Documental');
