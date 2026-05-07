9c4fb9da4b5f2226e027ad2055462226fb3cb204
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2026-05-07 17:55:27 -0400

    modificaciones para acceso limitado a bd

--- ms-mantenedores/routes/areaRoutes.js
+++ ms-mantenedores/routes/areaRoutes.js
@@ -1,19 +1,26 @@
-const express = require('express');
-const router = express.Router({ mergeParams: true });
+const express    = require('express');
+const rateLimit  = require('express-rate-limit');
+const router     = express.Router({ mergeParams: true });
 
-const areaCRUD = require('../controllers/areaController');
+const areaCRUD   = require('../controllers/areaController');
 const areaUsuario = require('../controllers/areaUsuarioController');
 
-router.get('/',                        areaCRUD.getAreas);
-router.post('/',                       areaCRUD.crearArea);
-router.put('/:id',                     areaCRUD.editarArea);
-router.delete('/:id',                  areaCRUD.desactivarArea);
-router.patch('/:id/reactivar',         areaCRUD.reactivarArea);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
-router.get('/:areaId/usuarios',                  areaUsuario.getUsuariosPorArea);
-router.get('/:areaId/usuarios-disponibles',      areaUsuario.getUsuariosDisponibles);
-router.post('/:areaId/usuarios',                 areaUsuario.asignarUsuario);
-router.delete('/:areaId/usuarios/:asignacionId', areaUsuario.removerUsuario);
+router.get('/',               limiter, areaCRUD.getAreas);
+router.post('/',              limiter, areaCRUD.crearArea);
+router.put('/:id',            limiter, areaCRUD.editarArea);
+router.delete('/:id',         limiter, areaCRUD.desactivarArea);
+router.patch('/:id/reactivar',limiter, areaCRUD.reactivarArea);
 
+router.get('/:areaId/usuarios',              limiter, areaUsuario.getUsuariosPorArea);
+router.get('/:areaId/usuarios-disponibles',  limiter, areaUsuario.getUsuariosDisponibles);
+router.post('/:areaId/usuarios',             limiter, areaUsuario.asignarUsuario);
+router.delete('/:areaId/usuarios/:asignacionId', limiter, areaUsuario.removerUsuario);
 
 module.exports = router;
