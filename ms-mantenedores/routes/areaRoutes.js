9d2410bc4d3a3fee687f3bfff2ba272b504040a2
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2026-05-21 01:09:13 -0400

    Agrega ruta para proceso registrado

--- ms-mantenedores/routes/areaRoutes.js
+++ ms-mantenedores/routes/areaRoutes.js
@@ -14,11 +14,12 @@ const limiter = rateLimit({
 
 router.get('/',               limiter, areaCRUD.getAreas);
 router.post('/',              limiter, areaCRUD.crearArea);
-// Ruta específica ANTES de las rutas con parámetro /:id
+
 router.get('/mis-unidades',   limiter, areaUsuario.getMisUnidades);
 router.put('/:id',            limiter, areaCRUD.editarArea);
 router.delete('/:id',         limiter, areaCRUD.desactivarArea);
 router.patch('/:id/reactivar',limiter, areaCRUD.reactivarArea);
+router.patch('/:id/proceso',  limiter, areaCRUD.asignarProceso);   // HU-13
 
 router.get('/:areaId/usuarios',              limiter, areaUsuario.getUsuariosPorArea);
 router.get('/:areaId/usuarios-disponibles',  limiter, areaUsuario.getUsuariosDisponibles);
